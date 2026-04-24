14bd0b7e186caa01e832f5ed3a92c6b94b042b63
naufalfajar-maker <naufalfajarti@gmail.com>
2026-04-24 22:24:14 +0700

    vid 15

--- app/Controllers/Kategori.php
+++ app/Controllers/Kategori.php
@@ -2,14 +2,22 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
+use App\Models\ModelKategori;
 class Kategori extends BaseController
 {
+      public function __construct() 
+    {
+    $this->ModelKategori = new ModelKategori();
+    }
+
     public function index()
     {
         $data = [
             'judul' => 'Kategori',
             'menu' => 'kategori',
             'page' => 'v_kategori',
+            'kategori' => $this->ModelKategori->AllData(),
         ];
         return view('v_template_back_end', $data);
     }
