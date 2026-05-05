dddf1a7d4b2e34efd5c77f8a5b1fc5d94051347d
naufalfajar-maker <naufalfajarti@gmail.com>
2026-05-05 21:05:50 +0700

    vid 18

--- app/Controllers/Bengkel.php
+++ app/Controllers/Bengkel.php
@@ -2,15 +2,30 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
+use App\Models\ModelWilayah;
+use App\Models\ModelSetting;
+use App\Models\ModelBengkel;
+
 class Bengkel extends BaseController
 {
+    public function __construct() 
+    {
+    $this->ModelWilayah = new ModelWilayah();
+    $this->ModelSetting = new ModelSetting();
+    $this->ModelBengkel = new ModelBengkel();
+    }
+
     public function index()
     {
         $data = [
             'judul' => 'Bengkel',
             'menu' => 'bengkel',
             'page' => 'bengkel/v_index',
+            'bengkel' => $this->ModelBengkel->AllData(),
         ];
         return view('v_template_back_end', $data);
     }
+
+
 }
---
+++ app/Models/ModelBengkel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModelBengkel extends Model
+{
+    public function AllData()
+    {
+        return $this->db->table('tbl_bengkel')
+                ->get()->getResultArray();
+    }
+
+    public function InsertData($data)
+    {
+        $this->db->table('tbl_bengkel')->insert($data);
+    }
+
+    public function DetailData($id_bengkel)
+    {
+        return $this->db->table('tbl_bengkel')
+                ->where('id_bengkel', $id_bengkel)
+                ->get()->getRowArray();
+    }
+
+    public function UpdateData($data)
+    {
+        $this->db->table('tbl_bengkel')
+        ->where('id_bengkel', $data['id_bengkel'])
+        ->update($data);
+    }
+
+    public function DeleteData($data)
+    {
+        $this->db->table('tbl_bengkel')
+        ->where('id_bengkel', $data['id_bengkel'])
+        ->delete($data);
+    }
+}
